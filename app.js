ffd3f065f8f4e2a41c57bd243d1cf62d9b1f7a36
brandonluffman <77361960+brandonluffman@users.noreply.github.com>
2022-07-27 11:53:12 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+const myTags = [
+    'React', 'CSS', 'HTML',
+    'Javascript', 'Node.js', 'MongoDB',
+    'Express.js', 'Bootstrap','Typescript',
+    'npm', 'jQuery','Sass', 
+    'Webpack', 'Git', 'Redux',
+    'Jest', 'MySQL'
+];
+
+var tagCloud = TagCloud('.content', myTags,{
+
+  // radius in px
+  radius: 200,
+
+  // animation speed
+  // slow, normal, fast
+  maxSpeed: 'normal',
+  initSpeed: 'normal',
+
+  // 0 = top
+  // 90 = left
+  // 135 = right-bottom
+  direction: 135,
+  
+  // interact with cursor move on mouse out
+  keep: true
+  
+});
+
+//To change the color of text randomly
+var colors = ['#8ac7ff'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('.content').style.color = random_color;
+
+
+
+
+
+// Nav Scroll
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-95px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+
+
+
+
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Full-Stack Engineer', 'React Developer', 'Web Developer'], 'text',['white','white','white']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
+
+
+
+
+
+
+
+
+
+// Navbar Menu
+
+
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+
+
+
+// Fades
+
+
